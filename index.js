2fbb095ec6b927f87886688a19820c0e7fd7183b
Wan Chang Lai <wanchang.lai@live.com>
2022-11-30 13:48:00 +0800

    Create Object Using Object Literal

---
+++ index.js
@@ -0,0 +1,14 @@
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+// if a member is a function, we defined it as a method
+
+circle.draw();
