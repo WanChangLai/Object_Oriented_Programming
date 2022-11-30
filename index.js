8445b8abd49e83627fcc193c102ccb0e3b2254b6
Wan Chang Lai <wanchang.lai@live.com>
2022-11-30 14:00:25 +0800

    Creating Object Using Factory Function

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log("draw");
-  },
-};
+//this is a factory function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+
+const circle = createCircle(1);
 
 // if a member is a function, we defined it as a method
 
